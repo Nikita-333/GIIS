5a368eb5862cc565b38f040885f48d675710d992
Nikita-333 <krawchenko.nikita2016@gmail.com>
2023-10-19 03:53:25 +0300

    Files for build parametric curves

---
+++ ParamCurve.py
@@ -0,0 +1,133 @@
+import numpy as np
+from scipy import interpolate
+
+class ParamCurveAlgorithms:
+
+    def __init__(self, canvas):
+        self.selected_algorithm = None
+        self.canvas = canvas
+        self.start_point = None
+        self.action_list = []
+        self.start_point = None
+        self.end_point = None
+        self.points = []
+        self.is_drawing = False
+
+    def add_action(self, action):
+        self.action_list.append(action)
+
+    def right_click_point(self, event):
+        if self.is_drawing:
+            return
+        self.points.append((event.x, event.y))
+        self.canvas.create_oval(event.x - 2, event.y - 2, event.x + 2, event.y + 2, fill="red")
+        if len(self.points) >= 4:
+            self.is_drawing = True  # Set the state to drawing
+
+    def left_click_draw(self, event):
+        if not self.is_drawing:
+            return
+        self.draw_b_spline()
+        self.is_drawing = False  # Reset the state
+
+    def draw_b_spline(self):
+        if len(self.points) < 4:
+            print("Для кривой B-сплайн требуется минимум 4 точки.")
+            return
+
+        x = [point[0] for point in self.points]
+        y = [point[1] for point in self.points]
+
+        tck, u = interpolate.splprep([x, y], s=0)
+        unew = np.linspace(0, 1.0, 1000)
+        out = interpolate.splev(unew, tck)
+
+        for i in range(len(out[0]) - 1):
+            self.canvas.create_line(out[0][i], out[1][i], out[0][i + 1], out[1][i + 1])
+
+        self.points = []
+    def draw_hermit(self, event):
+        if self.start_point is None:
+            self.start_point = (event.x, event.y)
+            self.add_action("Форма для Хермит: Начальная точка: ({}, {})".format(event.x, event.y))
+            return
+
+        end_point = (event.x, event.y)
+        self.add_action("Форма для Хермит: Конечная точка: ({}, {})".format(event.x, event.y))
+        self.draw_hermit_algo(self.start_point, end_point)
+        self.start_point = None
+
+    def draw_hermit_algo(self, start_point, end_point):
+
+        x0, y0 = start_point
+        x1, y1 = end_point
+
+        tangent_start = (200, 0)
+        tangent_end = (0, -200)
+
+        for t in range(0, 101):
+            t /= 100.0
+            t2 = t * t
+            t3 = t2 * t
+            x = (2 * t3 - 3 * t2 + 1) * x0 + (t3 - 2 * t2 + t) * tangent_start[0] + (-2 * t3 + 3 * t2) * x1 + (
+                        t3 - t2) * tangent_end[0]
+            y = (2 * t3 - 3 * t2 + 1) * y0 + (t3 - 2 * t2 + t) * tangent_start[1] + (-2 * t3 + 3 * t2) * y1 + (
+                        t3 - t2) * tangent_end[1]
+            self.canvas.create_rectangle(x, y, x + 1, y + 1)
+
+    def draw_bezier_curve(self, event):
+        if self.start_point is None:
+            self.start_point = (event.x, event.y)
+            self.add_action("Форма для Безье: Начальная точка: ({}, {})".format(event.x, event.y))
+            return
+
+        if self.end_point is None:
+            self.end_point = (event.x, event.y)
+            self.add_action("Форма для Безье: Конечная точка: ({}, {})".format(event.x, event.y))
+            return
+
+        # Создаем две дополнительные точки
+        control_point1 = (event.x, self.start_point[1])
+        control_point2 = (self.end_point[0], event.y)
+
+        # Формируем список из четырех контрольных точек
+        control_points = [self.start_point, control_point1, control_point2, self.end_point]
+        self.draw_bezier_curve_algo(control_points)
+        self.start_point = None
+        self.end_point = None
+
+    def draw_bezier_curve_algo(self, control_points):
+        if len(control_points) != 4:
+            print("Для кривой Безье требуется 4 контрольные точки.")
+            return
+
+        P0, P1, P2, P3 = control_points
+
+        steps = 100
+        delta_t = 1.0 / steps
+
+        x_coords = []
+        y_coords = []
+
+        for i in range(steps + 1):
+            t = i * delta_t
+            u = 1 - t
+            x = u ** 3 * P0[0] + 3 * u ** 2 * t * P1[0] + 3 * u * t ** 2 * P2[0] + t ** 3 * P3[0]
+            y = u ** 3 * P0[1] + 3 * u ** 2 * t * P1[1] + 3 * u * t ** 2 * P2[1] + t ** 3 * P3[1]
+
+            x_coords.append(x)
+            y_coords.append(y)
+
+        for i in range(steps):
+            x1, y1 = x_coords[i], y_coords[i]
+            x2, y2 = x_coords[i + 1], y_coords[i + 1]
+            self.canvas.create_line(x1, y1, x2, y2)
+
+    def select_bezier_curve(self):
+        self.selected_algorithm = self.draw_bezier_curve
+
+    def select_hermit_algorithm(self):
+        self.selected_algorithm = self.draw_hermit
+
+    def select_spline(self):
+        self.selected_algorithm=self.draw_b_spline
